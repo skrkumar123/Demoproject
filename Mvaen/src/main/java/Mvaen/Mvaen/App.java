1b97154a20c56f8d411c56efd94478a2d77fbcef
Sonu <Sonu@LAPTOP-AF9EHLIC>
2022-02-16 19:56:38 +0530

    First commit

---
+++ Mvaen/src/main/java/Mvaen/Mvaen/App.java
@@ -0,0 +1,13 @@
+package Mvaen.Mvaen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mvaen/src/test/java/test/Testmethod.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Testmethod {
+
+	@Test
+	public void getuser()
+	{
+		
+		System.out.println("welcome sonu");
+	}
+	
+	
+	
+	
+}
